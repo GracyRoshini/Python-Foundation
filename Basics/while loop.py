954d979b6637b139d7356b6a780a53f442496b42
GracyRoshini <120003405+GracyRoshini@users.noreply.github.com>
2025-07-27 13:04:28 +0530

    Add files via upload

---
+++ Basics/while loop.py
@@ -0,0 +1,12 @@
+#--WHILE LOOP
+count=0
+sum=0
+
+while count<=5:
+    num=int(input("Enter number:"))
+    sum=sum+num
+    count=count+1
+
+print("sum:", sum)
+print("count:", count)
+print("avg:",sum/6)
