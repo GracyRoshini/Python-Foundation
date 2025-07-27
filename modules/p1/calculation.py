3d2cef0ed22b0e3ef39d7891fde1cec87d3455a6
GracyRoshini <120003405+GracyRoshini@users.noreply.github.com>
2025-07-27 12:49:14 +0530

    Add files via upload

---
+++ modules/p1/calculation.py
@@ -0,0 +1,9 @@
+#--modules
+
+def add(a,b):
+    c=a+b
+    return c
+
+def sub(a,b):
+    c=a-b
+    return c
---
+++ modules/p1/call_calculation.py
@@ -0,0 +1,10 @@
+import calculation
+import employee #--called file name
+
+res_add=calculation.add(2,3)
+res_sub=calculation.sub(5,2)
+
+print("add: ",res_add)
+print("sub: ",res_sub)
+print("Employee: ",emp_name)
+print("Salary: ",emp_sal)
